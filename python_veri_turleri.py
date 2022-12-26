e0e61908e2917b70c3a7946ab08c8e79f706c10c
Kerim DELİL <46048662+delilkerim@users.noreply.github.com>
2022-12-26 18:23:45 +0300

    Add files via upload

---
+++ python_veri_turleri.py
@@ -0,0 +1,19 @@
+# Pyhtonda tam sayı
+print(12)
+print(type(12))
+print("--------------------------------")
+# Pythonda ondalıklı sayı
+print(15.25)
+print(type(15.25))
+print("--------------------------------")
+# Pythonda String değerler
+print("Merhaba Dünya-Çift Tırnaklı")
+print('Merhaba Dünya-Tek Tırnaklı')
+print(type('Merhaba Dünya-Tek Tırnaklı'))
+print("--------------------------------")
+# Pythonda Bool değerler
+print(True)
+print(False)
+print(type(True))
+print("--------------------------------")
+
