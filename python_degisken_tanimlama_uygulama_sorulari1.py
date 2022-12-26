6bac8097b315f12ec96e1c4d4bff04a063d19ce1
Kerim DELİL <46048662+delilkerim@users.noreply.github.com>
2022-12-26 19:19:42 +0300

    Add files via upload

---
+++ python_degisken_tanimlama_uygulama_sorulari1.py
@@ -0,0 +1,40 @@
+"""
+1- Bir öğrencinin aşağıdaki bilgileri için değişkenler oluşturunuz ve değerlerini giriniz.
+    Öğrenci Adı
+    Öğrenci Soyadı
+    Öğrenci Adı + Soyadı
+    Öğrenci Cinsiyet
+    Öğrenci T.C. Kimlik
+    Öğrenci Doğum Yılı
+    Öğrenci Adres Bilgisi
+    Öğrenci Yaşı
+"""
+ogrAd = "Kerim"
+ogrSoyad = "DELİL"
+ogrAdSoyad = ogrAd + ogrSoyad
+ogrCinsiyet = "Erkek"
+ogrTCNo = "2321321332"
+ogrDogumYili = 1980
+ogrAdresBilgisi = "Aşağı Söğütönü Mah. 1541. Sokak Toki Konutları Tepebaşı/ESKİŞEHİR"
+ogrYas = 42
+
+print(ogrAdSoyad)
+print(ogrCinsiyet)
+print(ogrTCNo)
+print(ogrDogumYili)
+print(ogrAdresBilgisi)
+print(ogrYas)
+print("-------------------------------")
+
+'''
+    Bir pizzacıya gelen siparişlerin toplam fiyatını değişkenler kullanarak hesaplayınız.
+    sipariş-1: 35 TL
+    sipariş-2: 68 TL
+    sipariş-3: 82 TL
+'''
+siparis1 = 35
+siparis2 = 68
+siparis3 = 82
+siparisToplam = siparis1 + siparis2 + siparis3
+print(siparisToplam)
+print("-------------------------------")
